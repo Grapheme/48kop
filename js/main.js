00240b830d7a3d8298201e85cf74f1881e1fafb9
KostyaD <thedamaxstudio@gmail.com>
2014-06-13 17:25:20 +0400

    form

--- js/main.js
+++ js/main.js
@@ -1,1 +1,25 @@
+$(document).on('focus', '.age input', function(){
+	$(this).parent().addClass('active');
+});
+$(document).on('focusout', '.age input', function(){
+	$(this).parent().removeClass('active');
+});
 
+
+$(document).on('mouseover', '.family-item', function(){
+	$(this).find('.person').fadeIn();
+});
+$(document).on('mouseleave', '.family-item', function(){
+	$(this).find('.person').fadeOut();
+});
+
+var Family = (function(){
+	var count = 0;
+
+	var str = [];
+
+	str['father'] = '';
+	str['mother'] = '';
+	str['boy'] = '<span class="age"><input name="age" maxlength="2" type="text"><label class="fill-age">Укажите возраст сына</label></span>';
+	str['gril'] = '<span class="age"><input name="age" maxlength="2" type="text"><label class="fill-age">Укажите возраст дочери</label></span>';
+})();
