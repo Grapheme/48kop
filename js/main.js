deb79f84e10f4b6ddfcae3883f777bfd4c166a2f
Artem Tumin <arttumin@gmail.com>
2014-06-16 18:30:40 +0400

    popup rec fadeIn

--- js/main.js
+++ js/main.js
@@ -200,4 +200,9 @@ var App = (function(){
 		e.preventDefault();
 		$(this).parent().html('<span class="success">Ваша рекомендация успешно<br>отправлена вашему другу</span>');
 	});
+	$(document).on('click', '.places-link, .events-link, .advices-head > a', function(e){
+		e.preventDefault();
+		$('.overlay').removeClass('hidden');
+		$('.popup.rec').removeClass('hidden');
+	});
 })();
